4546245b7c67e6c244e6239a315b5a2d39524eff
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-08-29 19:26:34 +0200

    wtf

--- server.js
+++ server.js
@@ -1206,7 +1206,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  
+  console.log(`Server is listening on port ${PORT}`)
 });
 
 const GRID_SIZE = 15;
